ae66f507e1cb23208d072794d398f0ccb8a35bd2
bikejojo <jorgecsri@gmail.com>
2024-06-11 22:18:21 -0400

    queries_v2

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -101,6 +101,80 @@ export const GET_PETS_BY_USER_ID = gql`
   }
 `;
 
+export const GET_ALL_VISITS = gql`
+  query GetAllVisits {
+    getAllVisits {
+      id
+      date
+      reason
+      idPatient
+      idDoctor
+      status
+      reserved
+    }
+  }
+`;
+
+export const GET_VISITS_BY_DOCTOR = gql`
+  query GetVisitsByDoctor($idDoctor: String!) {
+    getVisitsByDoctor(idDoctor: $idDoctor) {
+      id
+      date
+      reason
+      idPatient
+      idDoctor
+      status
+      reserved
+    }
+  }
+`;
+
+export const GET_CONSULTATIONS_BY_ID_PATIENT = gql`
+  query GetConsultationsByIdPatient($id: ID!) {
+    getConsultationsByIdPatient(id: $id) {
+      id
+      patientId
+      doctorId
+      date
+      weight
+      height
+      temperature
+      heartRate
+      respiratoryRate
+      diagnosis
+      treatment
+      notes
+    }
+  }
+`;
+
+
+export const GET_ALL_VACCINES = gql`
+  query GetAllVaccines {
+    getAllVaccines {
+      id
+      patientId
+      vaccineName
+      administeredDate
+      nextAdministeredDate
+      doses
+    }
+  }
+`;
+
+export const GET_VACCINE_BY_ID_PATIENT = gql`
+  query GetVaccineByIdPatient($id: ID!) {
+    getVaccineByIdPatient(id: $id) {
+      id
+      patientId
+      vaccineName
+      administeredDate
+      nextAdministeredDate
+      doses
+    }
+  }
+`;
+
 // Mutaciones
 export const CREATE_ROL = gql`
   mutation CreateRol($rolRequest: RolRequest!) {
@@ -171,5 +245,77 @@ export const UPDATE_PET = gql`
 export const DELETE_PET = gql`
   mutation DeletePet($id: ID!) {
     deletePet(id: $id)
+  }
+`;
+
+export const CREATE_VISITS = gql`
+  mutation CreateVisits($visitsRequest: VisitsRequest!) {
+    createVisits(visitsRequest: $visitsRequest) {
+      id
+      date
+      reason
+      idPatient
+      idDoctor
+      status
+      reserved
+    }
+  }
+`;
+
+export const UPDATE_VISITS = gql`
+  mutation UpdateVisits($id: ID!, $status: String) {
+    updateVisits(id: $id, status: $status) {
+      id
+      date
+      reason
+      idPatient
+      idDoctor
+      status
+      reserved
+    }
+  }
+`;
+
+export const DELETE_VISITS = gql`
+  mutation DeleteVisits($id: ID!) {
+    deleteVisits(id: $id)
+  }
+`;
+
+export const CREATE_CONSULTATIONS = gql`
+  mutation CreateConsultations($consultationsRequest: ConsultationsRequest!) {
+    createConsultations(consultationsRequest: $consultationsRequest) {
+      id
+      patientId
+      doctorId
+      date
+      weight
+      height
+      temperature
+      heartRate
+      respiratoryRate
+      diagnosis
+      treatment
+      notes
+    }
+  }
+`;
+
+export const DELETE_CONSULTATIONS = gql`
+  mutation DeleteConsultations($id: ID!) {
+    deleteConsultations(id: $id)
+  }
+`;
+
+export const CREATE_VACCINE = gql`
+  mutation CreateVaccine($vaccineRequest: VaccineRequest!) {
+    createVaccine(vaccineRequest: $vaccineRequest) {
+      id
+      patientId
+      vaccineName
+      administeredDate
+      nextAdministeredDate
+      doses
+    }
   }
 `;
